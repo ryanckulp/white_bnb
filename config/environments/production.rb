8fdba7a97cacb9eaf6da50f662b2ef563acb1fc7
Ryan Kulp <ryanckulp@gmail.com>
2024-09-29 21:18:05 -0400

    rubocop

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,8 +35,8 @@ Rails.application.configure do
 
   # Log to STDOUT with the current request id as a default log tag.
 
-  config.log_tags = [ :request_id ]
-  config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
+  config.log_tags = [:request_id]
+  config.logger   = ActiveSupport::TaggedLogging.logger($stdout)
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Only use :id for inspections in production.
-  config.active_record.attributes_for_inspect = [ :id ]
+  config.active_record.attributes_for_inspect = [:id]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
