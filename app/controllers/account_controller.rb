1fba56618dc295b95d957e404c763c837ee6e884
Ryan Kulp <ryanckulp@gmail.com>
2024-10-02 19:33:26 -0400

    fix checkout summary

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,6 +1,5 @@
 class AccountController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_flashes
 
   def index; end
 
@@ -23,9 +22,5 @@ class AccountController < ApplicationController
 
   def account_update_params
     params.require(:user).permit(:email, :password)
-  end
-
-  def set_flashes
-    flash.now[:notice] = 'Subscription updated successfully' if params[:updated] == 'true'
   end
 end
