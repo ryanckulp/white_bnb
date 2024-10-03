a1af8e6628940f390391959b4087fb7041df702e
Ryan Kulp <ryanckulp@gmail.com>
2024-10-02 20:24:52 -0400

    support for Coupons

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,10 +1,18 @@
 class PaymentsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_booking
-  skip_before_action :verify_authenticity_token, only: [:create]
+  before_action :set_coupon, only: [:create]
 
   def create
-    render json: create_payment_intent # /checkout view
+    payment_intent = Stripe::PaymentIntent.create(
+      amount: amount_in_cents,
+      customer: current_user.stripe_customer_id,
+      setup_future_usage: 'off_session',
+      currency: 'usd',
+      automatic_payment_methods: { enabled: true }
+    )
+
+    render json: { clientSecret: payment_intent.client_secret, amount: amount_in_dollars, nights: @booking.nights, addonFees: @booking.addon_fees, couponAmount: coupon_amount_in_dollars }
   end
 
   def confirm
@@ -20,15 +28,23 @@ class PaymentsController < ApplicationController
 
   private
 
-  def create_payment_intent
-    payment_intent = Stripe::PaymentIntent.create(
-      amount: @booking.total_amount * 100, # must be in cents
-      customer: current_user.stripe_customer_id,
-      setup_future_usage: 'off_session',
-      currency: 'usd',
-      automatic_payment_methods: { enabled: true }
-    )
+  def set_coupon
+    @coupon = begin
+      Stripe::Coupon.retrieve(params[:coupon]) if params.key?(:coupon)
+    rescue Stripe::InvalidRequestError
+      nil
+    end
+  end
 
-    { clientSecret: payment_intent.client_secret, amount: @booking.total_amount, nights: @booking.nights, addonFees: @booking.addon_fees }
+  def coupon_amount_in_dollars
+    ((@coupon&.amount_off || 0) / 100.0)
+  end
+
+  def amount_in_dollars
+    @booking.total_amount - coupon_amount_in_dollars
+  end
+
+  def amount_in_cents
+    (@booking.total_amount * 100) - (@coupon&.amount_off || 0)
   end
 end
