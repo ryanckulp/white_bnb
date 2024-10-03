729bf84c535e3e578b95689df47aaf7176cb6c9b
Ryan Kulp <ryanckulp@gmail.com>
2024-10-02 22:04:39 -0400

    more scrubbing

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "WhiteBnb"
+  config.site_title = Rails.application.credentials.company_name
 
   # == Load Paths
   #
