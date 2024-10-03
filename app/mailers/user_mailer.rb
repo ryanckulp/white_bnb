63b525d16407a0a3d920ec3839fda45f84d7a4e0
Ryan Kulp <ryanckulp@gmail.com>
2024-10-02 20:53:36 -0400

    fix tests

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,6 @@
 class UserMailer < ApplicationMailer
+  def welcome(user)
+    @user = user
+    mail(to: user.email, subject: "Welcome to #{Rails.application.credentials.company_name}!")
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -74,7 +74,7 @@ RSpec.configure do |config|
   config.before(:each) do
     allow(Rails.application.credentials).to receive(:base_url).and_return('https://whitebnb.com')
     allow(Rails.application.credentials).to receive(:stripe).and_return(OpenStruct.new(api_key: 'sk_asdf', publishable_key: 'pk_asdf', product_price_id: 'pi_asdf'))
-    allow_any_instance_of(PaymentsController).to receive(:create_payment_intent).and_return({ client_secret: 'qwerty' }.to_json)
+    allow_any_instance_of(PaymentsController).to receive(:create).and_return({ client_secret: 'qwerty' }.to_json)
   end
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
