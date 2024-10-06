241407842debe401132ea518ede92e18eec96424
Ryan Kulp <ryanckulp@gmail.com>
2024-10-06 14:00:19 -0400

    separate guest booking domain for use elsewhere

--- app/models/concerns/onboardable.rb
+++ app/models/concerns/onboardable.rb
@@ -3,7 +3,11 @@ module Onboardable
 
   included do
     def self.create_guest
-      create!(email: "guest-#{Time.now.to_i}@#{Rails.application.credentials.company_name.parameterize}.com", password: SecureRandom.hex(10))
+      create!(email: "guest-#{Time.now.to_i}@#{guest_email_domain}", password: SecureRandom.hex(10))
+    end
+
+    def self.guest_email_domain
+      "#{Rails.application.credentials.company_name.parameterize}.com"
     end
   end
 end
