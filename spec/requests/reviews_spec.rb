446691c8b8c3d3e58a0683dbc9ee4b0085d04e8e
Ryan Kulp <ryanckulp@gmail.com>
2024-10-07 12:45:40 -0400

    review tests

--- spec/requests/reviews_spec.rb
+++ spec/requests/reviews_spec.rb
@@ -1,17 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe "Reviews", type: :request do
+  let(:valid_attributes) do
+    { review: build(:review).attributes.except('id', 'approved', 'created_at', 'updated_at') }
+  end
+
+  let(:invalid_attributes) do
+    { review: { title: 'title only' } }
+  end
+
   describe "GET /new" do
     it "returns http success" do
-      get "/reviews/new"
+      get new_review_path
       expect(response).to have_http_status(:success)
+      expect(response.body).to include('Leave a review')
+    end
+
+    it "pre-fills name and email" do
+      email = 'jim.bubb@gmail.com'
+      name = 'Jim Bob'
+
+      get new_review_path(email: email, name: name)
+
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include('Leave a review')
+      expect(response.body).to include(email)
+      expect(response.body).to include(name)
     end
   end
 
-  describe "GET /create" do
+  describe "POST /create" do
     it "returns http success" do
-      get "/reviews/create"
-      expect(response).to have_http_status(:success)
+      post "/reviews", params: valid_attributes
+
+      expect(response).to have_http_status(302)
+      follow_redirect! # redirects to home page
+
+      expect(response.body).to include('Thanks for your review')
+      expect(request.path).to eql '/'
+    end
+
+    it "returns validation errors" do
+      post "/reviews", params: invalid_attributes
+
+      expect(response).to have_http_status(422)
+      expect(response.body).to include('errors prohibited this review from being saved')
+      expect(response.body).to include("can&#39;t be blank")
     end
   end
 end
